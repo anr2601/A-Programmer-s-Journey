58f9bb6f81eedc4a1ec9cc53003b88d099e41799
NamelessExplorer <arx3099@gmail.com>
2022-05-29 18:42:09 +0530

    Improvised the code using Kadane's Algorithm

--- DSA/maxsub.java
+++ DSA/maxsub.java
@@ -7,34 +7,34 @@ public class maxsub {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size of the array: ");
         int n = sc.nextInt();
-        int[] arr= new int[n];
-        int[] sum=new int[n];
+        int[] arr = new int[n];
 
         System.out.println("Enter values for the array: ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
+        sc.close();
 
         System.out.println("Original Array:");
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
-
         System.out.println();
+        System.out.println("Sum of largest contiguous subarray:"+ max(n, arr));
+    }
+
+    static int max(int n, int arr[]) {
         int max_till_now = Integer.MIN_VALUE;
         for (int i = 0; i < n; i++) {
             int current_max = 0;
-            for (int j = i; j < n; j++) {
-                current_max+=arr[i];
+            current_max += arr[i];
+            if (max_till_now < current_max) {
+                max_till_now = current_max;
             }
-            if(current_max>max_till_now){
-                max_till_now=current_max;
+            if (current_max < 0) {
+                current_max = 0;
             }
-            sum[i]=current_max;
         }
-        System.out.println("Sum of largest contiguous subarray:");
-        for(int i=0;i<n;i++){
-                System.out.print(sum[i]+" ");
-            }
+        return max_till_now;
     }
 }
