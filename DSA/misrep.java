0e42a1b4d4cb24fcee5232ccf76b50b7fd06477f
NamelessExplorer <arx3099@gmail.com>
2022-07-18 12:38:43 +0530

    Find the missing and repeating number

---
+++ DSA/misrep.java
@@ -0,0 +1,44 @@
+
+/*
+You are given a read only array of n integers from 1 to n.
+
+Each integer appears exactly once except A which appears twice and B which is missing.
+
+Return A and B.
+
+ */
+
+
+ import java.util.*;
+
+public class misrep{
+    public static void main(String[] args){
+    int arr[]=new int[5];
+    arr[0]=1;
+    arr[1]=2;
+    arr[2]=4;
+    arr[3]=5;
+    arr[4]=1;
+
+    Arrays.sort(arr);
+
+    for(int i=0;i<5;i++){
+        for(int j=i+1;j<5;j++){
+            if(arr[i]==arr[j]){
+                System.out.println("Repeating Number: "+ arr[i]);
+                break;
+            }
+        }
+    }
+
+    for(int i=1;i<(4);i++){
+        int x=arr[i+1]-1;
+        if(arr[i]!=x){
+            System.out.print("Missing Number: "+ x);
+            break;
+        }
+    }
+
+
+}
+}
