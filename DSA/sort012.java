58e4fd3335670720653b0ce8859ece192d8f9fc6
NamelessExplorer <arx3099@gmail.com>
2022-08-03 10:36:42 +0530

    Sort 0,1,2 in array

--- DSA/sort012.java
+++ DSA/sort012.java
@@ -6,7 +6,10 @@ class sort012{
         Scanner sc=new Scanner(System.in);
         int N=sc.nextInt();
         int arr[]= new int[N];
+
+
         int c0=0,c1=0,c2=0;
+        
         for(int i=0;i<N;i++){
             arr[i]=sc.nextInt();
             if(arr[i]==0){
@@ -24,6 +27,29 @@ class sort012{
             System.out.print(arr[i]+" ");
         }
 
+        System.out.println("\n\nSorting the array...");
+        for(int i=0;i<c0;i++){
+            arr[i]=0;
+        }
+
+        for(int i=c0;i<(c0+c1);i++){
+            arr[i]=1;
+        }
+
+        for(int i=(c0+c1);i<(c0+c1+c2);i++){
+            arr[i]=2;
+        }
+
+        System.out.println();
+
+        System.out.println("Array is sorted");
+
+        System.out.println();
+
+        for(int i=0;i<N;i++){
+            System.out.print(arr[i]+" ");
+        }
+        
 
     }
 }
