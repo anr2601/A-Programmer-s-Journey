0e08863837a83ae1d17dcbef030d1b5af20d2aed
NamelessExplorer <arx3099@gmail.com>
2022-08-20 16:47:25 +0530

    Added transition

--- Vanilla JS Projects/Weather App/style.js
+++ Vanilla JS Projects/Weather App/style.js
@@ -1,25 +1,12 @@
-var inputval = document.querySelector('#cityinput')
-var btn = document.querySelector('#add');
+window.onload = () => {
 
-apik = "3045dd712ffe6e702e3245525ac7fa38";
-
-function convertion(val){
-    return (val - 273).toFixed(2)
+    const submit = document.getElementById('add');
+    const container = document.getElementsByClassName('container')[0];
+    
+    submit.onclick = (e) => {
+        
+        container.style.transform = "translateX(-50%)";
+    
+    }
 }
 
-
-
-btn.addEventListener('click', function(){  
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-            'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/minutely?lat=35.5&lon=-78.5', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-        })
