5f75a10fd8426cdbdf236ebc06be0951eb861c26
NamelessExplorer <arx3099@gmail.com>
2022-08-23 21:42:07 +0530

    Can fetch basic weather info now

--- Vanilla JS Projects/Weather App/style.js
+++ Vanilla JS Projects/Weather App/style.js
@@ -1,3 +1,12 @@
+let temperature = document.querySelector(".temp");
+let summary = document.querySelector(".summary");
+let loc = document.querySelector(".location");
+let icon = document.querySelector(".icon");
+let city1=document.getElementById('cityinput');
+const kelvin = 273;
+const apikey = 'bdd29bebb51e5ada2443f186820d80e5';
+ 
+
 window.onload = () => {
 
     const submit = document.getElementById('add');
@@ -6,7 +15,37 @@ window.onload = () => {
     submit.onclick = (e) => {
         
         container.style.transform = "translateX(-50%)";
+        e.preventDefault();
+        
+        let city=city1.value;
+        
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`)
+        .then((response) => {
+            return response.json();
+          })
+
+          .then((data) => {
+            console.log(data);
+            temperature.textContent = 
+                Math.floor(data.main.temp - kelvin) + "°C";
+            summary.textContent = data.weather[0].description;
+            loc.textContent = data.name + "," + data.sys.country;
+            let icon1 = data.weather[0].icon;
+            icon.innerHTML = 
+                `<img src="icons/${icon1}.svg" style= 'height:10rem'/>`;
+          })
+
+          .catch((errorResponse) => {
+            if (errorResponse.text) { //additional error information
+              errorResponse.text().then( errorMessage => {
+                //errorMessage now returns the response body which includes the full error message
+              })
+            } else {
+              //no additional error information 
+            } 
+          });
     
     }
 }
+
 
