7f4f2672a1ba41d78f7e84a559768886f83e6a0c
NamelessExplorer <arx3099@gmail.com>
2022-08-26 19:33:50 +0530

    Removed the need of two extra arrays

--- DSA/ProductArray.java
+++ DSA/ProductArray.java
@@ -6,23 +6,23 @@ class ProductArray{
             return;
         }
 
-        int left[] = new int[n];
-        int right[] = new int[n];
         int prod[] = new int[n];
 
-        int i,j;
+        int i,temp=1;
 
-        left[0] = 1;
-        right[n-1] = 1;
+        for(i=0;i<n;i++){
+            prod[i]=temp;
+            temp*=arr[i];
+        }
 
-        for(i=1;i<n;i++)
-            left[i] = arr[i-1] * left[i-1];
-        
-        for(j=n-2;j>=0;j--)
-            right[j] = arr[j+1] * right[j+1]; 
 
-        for (i = 0; i < n; i++)
-            prod[i] = left[i] * right[i];
+        temp=1;
+
+        for(i=n-1;i>=0;i--){
+            prod[i]*=temp;
+            temp*=arr[i];
+        }
+
  
         for (i = 0; i < n; i++)
             System.out.print(prod[i] + " ");
