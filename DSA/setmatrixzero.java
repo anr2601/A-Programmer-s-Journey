4072bb93a7bc77ffee92361ab09c0d9bb209ac75
NamelessExplorer <arx3099@gmail.com>
2022-10-25 17:17:13 +0530

    Used a function as psvm became too big

--- DSA/setmatrixzero.java
+++ DSA/setmatrixzero.java
@@ -2,14 +2,18 @@ import java.util.*;
 
 public class setmatrixzero {
 
-    static void setZeroes(int[][] arr, int x, int y) {
+    static void setZeroes(int[][] temp, int[][] arr, int i, int j, int x, int y) {
 
-        for (int i = 0; i < x; i++) {
-            for (int j = 0; j < y; j++) {
-                System.out.print(arr[i][j] + " ");
-            }
-            System.out.println();
-        }
+        for (int k = 0; k < i; k++)
+                    {
+                        temp[k][y] = 0;
+                    }
+
+                    for (int k = 0; k < j; k++) 
+                    {
+                        temp[x][k] = 0;
+                    }
+
     }
 
     public static void main(String[] args) {
@@ -42,43 +46,48 @@ public class setmatrixzero {
 
         int temp[][] = new int[i][j];
 
-        for(int x=0;x<i;x++){
-            for(int y=0;y<j;y++){
+        for(int x=0;x<i;x++)
+        {
+            for(int y=0;y<j;y++)
+            {
                 temp[x][y] = 1;
             }
         }
 
         System.out.println();
 
-        for (int x = 0; x < i; x++) {
-            for (int y = 0; y < j; y++) {
+        for (int x = 0; x < i; x++) 
+        {
+            for (int y = 0; y < j; y++) 
+            {
 
-                if (arr[x][y] == 0) {
+                if (arr[x][y] == 0) 
+                {
 
-                    for (int k = 0; k < i; k++)
-                    {
-                        temp[k][y] = 0;
-                    }
-
-                    for (int k = 0; k < j; k++) 
-                    {
-                        temp[x][k] = 0;
-                    }
+                    setZeroes(temp, arr, i, j, x, y);
 
                 }
 
             }
         }
-        for (int x = 0; x < i; x++) {
-            for (int y = 0; y < j; y++) {
+
+        for (int x = 0; x < i; x++)
+        {
+            for (int y = 0; y < j; y++) 
+            {
+
                 arr[x][y] = temp[x][y];
+            
             }
         }
 
-        for (int x = 0; x < i; x++) {
-            for (int y = 0; y < j; y++) {
+        for (int x = 0; x < i; x++) 
+        {
+            for (int y = 0; y < j; y++) 
+            {
                 System.out.print(arr[x][y]+" ");
             }
+            
             System.out.println();
         }
 
