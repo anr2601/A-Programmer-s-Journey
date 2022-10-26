370f8238bb1ba7bc3dfb93886a01ccc7fddfd3b0
NamelessExplorer <arx3099@gmail.com>
2022-10-26 20:33:57 +0530

    Put the brute force within a method

--- DSA/setmatrixzero.java
+++ DSA/setmatrixzero.java
@@ -2,19 +2,59 @@ import java.util.*;
 
 public class setmatrixzero {
 
-    static void setZeroes(int[][] temp, int[][] arr, int i, int j, int x, int y) {
 
-        for (int k = 0; k < i; k++)
+    //Brute Force Technique
+
+    /*
+     * private static void setZeroes(int[][] arr, int i, int j) {
+
+        int temp[][] = new int[i][j];
+
+        for (int x = 0; x < i; x++) {
+            for (int y = 0; y < j; y++) {
+
+                temp[x][y] = 1;
+            
+            }
+        }
+
+        for (int x = 0; x < i; x++) {
+            for (int y = 0; y < j; y++) {
+
+                if (arr[x][y] == 0) {
+
+                    for (int k = 0; k < i; k++)
+
                     {
                         temp[k][y] = 0;
                     }
 
-                    for (int k = 0; k < j; k++) 
+                    for (int k = 0; k < j; k++)
+
                     {
                         temp[x][k] = 0;
                     }
 
+                }
+
+            }
+        }
+
+        for (int y = 0; y < j; y++) {
+
+            for (int x = 0; x < i; x++) {
+                if (temp[x][y] == 0) {
+
+                    arr[x][y] = 0;
+
+                }
+
+            }
+        }
+
     }
+     */
+    
 
     public static void main(String[] args) {
 
@@ -35,62 +75,25 @@ public class setmatrixzero {
             }
         }
 
-        System.out.println(); 
+        System.out.println();
 
         for (int x = 0; x < i; x++) {
             for (int y = 0; y < j; y++) {
                 System.out.print(arr[x][y] + " ");
             }
+
             System.out.println();
         }
 
-        int temp[][] = new int[i][j];
+        //setZeroes(arr, i, j);
 
-        for(int x=0;x<i;x++)
-        {
-            for(int y=0;y<j;y++)
-            {
-                temp[x][y] = 1;
+        for (int x = 0; x < i; x++) {
+            for (int y = 0; y < j; y++) {
+                System.out.print(arr[x][y] + " ");
             }
-        }
 
-        System.out.println();
-
-        for (int x = 0; x < i; x++) 
-        {
-            for (int y = 0; y < j; y++) 
-            {
-
-                if (arr[x][y] == 0) 
-                {
-
-                    setZeroes(temp, arr, i, j, x, y);
-
-                }
-
-            }
-        }
-
-        for (int x = 0; x < i; x++)
-        {
-            for (int y = 0; y < j; y++) 
-            {
-
-                arr[x][y] = temp[x][y];
-            
-            }
-        }
-
-        for (int x = 0; x < i; x++) 
-        {
-            for (int y = 0; y < j; y++) 
-            {
-                System.out.print(arr[x][y]+" ");
-            }
-            
             System.out.println();
         }
-
 
         sc.close();
     }
