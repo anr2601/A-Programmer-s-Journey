42d17887c442eb0ee71f54cfd681bd8f0bb24f20
NamelessExplorer <91269749+NamelessExplorer@users.noreply.github.com>
2022-11-12 20:41:46 +0530

    Finished setting up the basic routes

--- React Projects/news-wave/src/App.js
+++ React Projects/news-wave/src/App.js
@@ -1,18 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
-import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import Navbar from './Components/Navbar';
 import News from './Components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
-      <Navbar/>
-      <News pageSize={5} country='in' category='science'/>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/"><News pageSize={5} country='in' category='general' /></Route>
+            <Route exact path="/business"><News pageSize={5} country='in' category='business' /></Route>
+            <Route exact path="/entertainment"><News pageSize={5} country='in' category='entertainment' /></Route>
+            <Route exact path="/general"><News pageSize={5} country='in' category='general' /></Route>
+            <Route exact path="/health"><News pageSize={5} country='in' category='health' /></Route>
+            <Route exact path="/science"><News pageSize={5} country='in' category='science' /></Route>
+            <Route exact path="/sports"><News pageSize={5} country='in' category='sports' /></Route>
+            <Route exact path="/technology"><News pageSize={5} country='in' category='technology' /></Route>
+          </Switch>
+
+
+
+        </Router>
+
       </div>
     )
   }
