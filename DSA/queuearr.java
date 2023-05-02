27f5e757df8722e4b89a8c58dc2c600942a6a219
NamelessExplorer <91269749+NamelessExplorer@users.noreply.github.com>
2023-05-03 00:00:11 +0530

    Operations to be performed

--- DSA/queuearr.java
+++ DSA/queuearr.java
@@ -2,6 +2,7 @@ class Queue {
 
     private int arr[];
     private int start, end, currSize, maxSize;
+
     public Queue() {
         arr = new int[16];
         start = -1;
@@ -16,6 +17,7 @@ class Queue {
         end = -1;
         currSize = 0;
     }
+
     public void push(int newElement) {
         if (currSize == maxSize) {
             System.out.println("Queue is full\nExiting...");
@@ -30,12 +32,16 @@ class Queue {
         System.out.println("The element pushed is " + newElement);
         currSize++;
     }
+
     public int pop() {
+        
         if (start == -1) {
             System.out.println("Queue Empty\nExiting...");
             System.exit(1);
         }
+        
         int popped = arr[start];
+        
         if (currSize == 1) {
             start = -1;
             end = -1;
@@ -64,6 +70,7 @@ public class queuearr {
         q.push(14);
         q.push(24);
         q.push(34);
+        System.out.println("Operations to be performed");
         System.out.println("The peek of the queue before deleting any element " + q.top());
         System.out.println("The size of the queue before deletion " + q.size());
         System.out.println("The first element to be deleted " + q.pop());
