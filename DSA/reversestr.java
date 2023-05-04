cbc0de351cb62c9277861273d80f7ddebce29767
NamelessExplorer <91269749+NamelessExplorer@users.noreply.github.com>
2023-05-04 23:31:20 +0530

    Put my leetcode solution for reversing words in a string

---
+++ DSA/reversestr.java
@@ -0,0 +1,32 @@
+class reversestr{
+    public String reverseWords(String s) {
+        
+        s+=" ";
+        
+        Stack<String> st = new Stack<String>();
+        int i;
+        String str = "";
+        
+        for (i = 0;i < s.length();i++)
+        {
+            if (s.charAt(i) == ' ')
+            {
+                st.push(str);
+                str = "";
+            }
+            else
+            {
+                str += s.charAt(i);
+            }
+        }
+        String ans = "";
+        
+        while (st.size() != 1)
+        {
+            ans += st.peek() + " ";
+            st.pop();
+        }
+        ans += st.peek(); // The last word should'nt have a space after it
+        return ans;
+}
+}
