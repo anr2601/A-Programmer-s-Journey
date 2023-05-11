576da5d9b5d9b38afae76008fcc12caa7b1ca2de
NamelessExplorer <91269749+NamelessExplorer@users.noreply.github.com>
2023-05-12 01:07:01 +0530

    Solved the Roman to Integer String problem

---
+++ DSA/romtoint.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class romtoint {
+    public static int romtoint(String s) {
+        int c = 0, num = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            switch (s.charAt(i)) {
+
+                case 'I':
+                    num = 1;
+                    break;
+
+                case 'V':
+                    num = 5;
+                    break;
+
+                case 'X':
+                    num = 10;
+                    break;
+
+                case 'L':
+                    num = 50;
+                    break;
+
+                case 'C':
+                    num = 100;
+                    break;
+
+                case 'D':
+                    num = 500;
+                    break;
+
+                case 'M':
+                    num = 1000;
+                    break;
+            }
+
+            if (4 * num < c)
+                c -= num;
+            else
+                c += num;
+
+        }
+
+        return c;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(romtoint(s));
+        sc.close();
+    }
+}
